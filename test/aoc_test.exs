3e327fd1a03654f0e5a6cddf7f6826d125e6523b
Alan Andrade <alan.andradec@gmail.com>
2015-12-12 20:42:15 -0800

    day 2

--- test/aoc_test.exs
+++ test/aoc_test.exs
@@ -6,17 +6,17 @@ defmodule AocTest do
 
   defmodule Instructions do
     def read input do
-      #chars_with_position
-      Enum.reduce(Enum.with_index(String.graphemes(input)),
-        0,
-        fn ({ char , pos }, floor) ->
-          case char do
+      input |>
+      String.graphemes |>
+      Enum.with_index |>
+      Enum.reduce(0, fn ({ char , pos }, floor) ->
+        case char do
           "(" -> floor + 1
           ")" ->
             if floor == 0,  do: IO.puts pos + 1
 
             floor - 1
-          end
+        end
       end)
     end
   end
@@ -34,4 +34,67 @@ defmodule AocTest do
   { :ok, input } = File.read("inputs/day1")
   IO.puts Instructions.read(input)
   IO.puts "END"
+
+
+  # Day 2
+
+  defmodule Elves do
+    def parse input do
+      input |>
+      String.split("x") |>
+      Enum.map(fn (x) -> String.to_integer(x) end)
+    end
+
+    def paper [l, w, h] do
+      x =  2*l*w + 2*w*h + 2*h*l
+      [a1, a2, a3] = [l*w, w*h, h*l]
+
+      x + min(a1, min(a2, a3))
+    end
+
+    def paper input do
+      input |> parse |> paper
+    end
+
+    def ribbon [l, w, h] do
+      # Shorter distance around its sides
+      # or smallest 
+      x = 2*l + 2*w
+      y = 2*h + 2*l
+      z = 2*h + 2*w
+
+      min(x, min(y, z)) + (l * w * h)
+    end
+
+    def ribbon input do
+      input |> parse |> ribbon
+    end
+
+    def remove_empty(input) do
+      input |> Enum.filter(fn (x) -> x != "" end)
+    end
+  end
+
+  test "paper calculation" do
+    assert Elves.paper("2x3x4") == 58
+    assert Elves.paper("1x1x10") == 43
+  end
+
+  test "ribbon calculation" do
+    assert Elves.ribbon("2x3x4") == 34
+    assert Elves.ribbon("1x1x10") == 14
+  end
+
+  { :ok, presents } = File.read("inputs/day2")
+  input = String.split(presents, "\n")
+
+  input |>
+  Enum.reduce(0, fn (input, acc) ->
+    if input == "" do
+      acc
+    else
+      acc + Elves.ribbon input
+    end
+  end) |>
+  IO.puts
 end
